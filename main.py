faf24b8a365c091f5b5516c196f5c52bc361577b
ChibaTomoki <michiamo.t1@gmail.com>
2023-05-06 15:33:35 +0900

    train.pyで作成したmodel.pthを使って、推論する結果を返すAPIの作成

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from base64 import b64decode
 from io import BytesIO
-from PIL import Image as PILImage
+from PIL import Image as PILImage, ImageOps as PILImageOps
 from fastapi.middleware.cors import CORSMiddleware
+import torch
+from torch import nn
+from torchvision.transforms import ToTensor
 
 app = FastAPI()
 origins = [
@@ -22,17 +25,46 @@ class Image(BaseModel):
     image_base64: str
 
 
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+nn_model = NeuralNetwork().to("cpu")
+trained_model = torch.load("model.pth")
+nn_model.load_state_dict(trained_model)
+
+
 @app.get("/sample/")
 async def sample():
     return "sample"
 
 
-@app.post("/images/")
+@app.post("/analyze-images/")
 async def create_image(image: Image):
-    image_data = b64decode(image.image_base64.split(",")[1])
+    img_bytes = b64decode(image.image_base64.split(",")[1])
+    img_bytes_io = BytesIO(img_bytes)
+    img = PILImage.open(img_bytes_io)
+    img_gray_scale_inverted = img.convert("L")
+    img_gray_scale = PILImageOps.invert(img_gray_scale_inverted)
+    img_resized = img_gray_scale.resize((28, 28))
+    img_tensor = ToTensor()(img_resized)
+    img_tensor_for_nn = img_tensor.unsqueeze(0)
 
-    image_file = BytesIO(image_data)
-    img = PILImage.open(image_file)
-    img.save("./posted_png_sample.png")
+    output = nn_model(img_tensor_for_nn)
+    prediction = torch.argmax(output, dim=1)
 
-    return {"message": "画像が正常に受信されました。"}
+    return prediction.item()
